de982fa9b34480961a0722c64c5b05494af61550
Ricardo Delgado <wrfamilia2022@gmail.com>
2024-05-21 19:20:56 -0300

    Com erro de ID

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { LoginService } from './login.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit,OnDestroy{
+  title = 'Sport Games';
+  mostrarMenu:boolean = true;
+
+  constructor(private _loginService: LoginService){}
+
+  ngOnInit(): void {
+    this._loginService.getMostraMenu().subscribe(
+      res => { this.mostrarMenu = res; }
+    )
+  }
+
+  ngOnDestroy(){
+    localStorage.clear();
+  }
+  
+}
